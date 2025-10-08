159953e55f92b1d9adac5340e19c140702331711
MennaAbdelatif <141871133+MennaAbdelatif@users.noreply.github.com>
2025-10-08 08:28:52 +0300

    Add files via upload

---
+++ keypad_driver.h
@@ -0,0 +1,15 @@
+/*
+ * keypad_driver.h
+ *
+ * Created: 2/24/2023
+ *  Author: Menna Abdellatif
+ */
+
+
+#ifndef KEYPAD_DRIVER_H_
+#define KEYPAD_DRIVER_H_
+#define NOTPRESSED 0xff
+#include "DIO.h"
+void keypad_vInit();
+char keypad_u8check_press();
+#endif /* KEYPAD_DRIVER_H_ */
